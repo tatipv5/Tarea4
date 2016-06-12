9ae743cb9ead1b5709233dfc33cfae421fe9bc91
tati <tati@tati-Inspiron-5545>
2016-06-11 20:09:56 -0600

    inicio de hilo, chooser hecho

--- Tarea4/src/hilos/Hilo.java
+++ Tarea4/src/hilos/Hilo.java
@@ -3,19 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tarea4;
+package hilos;
 
 /**
  *
  * @author tati
  */
-public class Tarea4 {
+public class Hilo {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public Hilo() {
     }
     
 }
---
+++ Tarea4/src/hilos/Lock.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hilos;
+
+/**
+ *
+ * @author tati
+ */
+public class Lock {
+    private boolean isLocked = false;
+    
+    public synchronized void lock()
+            throws InterruptedException{
+        while(isLocked){
+            wait();
+        }
+        isLocked = true;
+    }
+    
+    public synchronized void unlock(){
+        isLocked = false;
+        notify();
+    }
+    
+}
